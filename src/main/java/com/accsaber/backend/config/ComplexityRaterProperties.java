907a582b4d8e7eba938189244f497f55adf30405
Tikugato <tikugato@gmail.com>
2026-09-11 16:33:28 +0200

    fix: board slope containing non-board numbers

--- src/main/java/com/accsaber/backend/config/ComplexityRaterProperties.java
+++ src/main/java/com/accsaber/backend/config/ComplexityRaterProperties.java
@@ -15,7 +15,7 @@ import lombok.Data;
 @ConfigurationProperties(prefix = "accsaber.complexity.rater")
 public class ComplexityRaterProperties {
 
-    private String version = "note-acc-2026-09-16";
+    private String version = "note-acc-2026-09-17";
     private double worstShare = 0.05;
     private Board board = new Board();
     private Map<String, Coefficients> categories = new LinkedHashMap<>();
